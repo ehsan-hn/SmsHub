40595162751858db2811a955e2932895e6d568d3
Eyson <eysoon2@gmail.com>
2025-11-10 17:20:54 +0330

    init commit

---
+++ SmsHub/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SmsHub project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SmsHub.settings')
+
+application = get_wsgi_application()
