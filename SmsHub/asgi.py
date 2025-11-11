9c18714b6691f2835c2a5d93dbac1413dd8a118d
Eyson <eysoon2@gmail.com>
2025-11-11 10:02:18 +0330

    feat: add ruff formater

--- SmsHub/asgi.py
+++ SmsHub/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SmsHub.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SmsHub.settings")
 
 application = get_asgi_application()
--- SmsHub/wsgi.py
+++ SmsHub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SmsHub.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SmsHub.settings")
 
 application = get_wsgi_application()
